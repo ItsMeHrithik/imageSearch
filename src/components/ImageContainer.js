30fc11776c09364f2f4c640c4905b98e27b1bfe0
Hrithik mahadik <hrithik_cr7@Hrithiks-MacBook-Air.local>
2021-07-21 19:18:11 +0530

    template for the project done

---
+++ src/components/ImageContainer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ImageContainer() {
+  return (
+    <div>
+      <h1>Image Container</h1>
+    </div>
+  );
+}
+
+export default ImageContainer;
---
+++ src/components/SearchBar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function SearchBar() {
+  return (
+    <div>
+      <h1>This is the search bar</h1>
+    </div>
+  );
+}
+
+export default SearchBar;
